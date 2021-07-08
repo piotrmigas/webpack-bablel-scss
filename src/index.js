3bd72a1075812f3bc30660aa3a97a074a8dd5492
piotrmigas <piotr.migas@icloud.com>
2021-07-08 14:38:28 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "main.scss";
+
+const App = () => {
+  return <h1>App</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
